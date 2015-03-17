789685f78d1afef8cb4b33a1229db759c562e492
drummyfish <tastyfish@seznam.cz>
2015-03-17 23:33:17 +0100

    top layer rendering

--- game.py
+++ game.py
@@ -62,6 +62,15 @@ class MapGridObject:
   OBJECT_ENEMY_FLYING = 5
   OBJECT_ENEMY_GROUND = 6
   OBJECT_PLAYER = 7
+  OBJECT_SPIKES = 8
+
+  ## Checks if the argument is a tile.
+  #
+  #  @return True if what is a tile, False otherwise
+
+  @staticmethod
+  def is_tile(what):
+    return what != None and what.object_type == MapGridObject.OBJECT_TILE
 
   ## Makes an instance of MapGridObject based on provided string.
   #
@@ -84,6 +93,8 @@ class MapGridObject:
       result.object_type = MapGridObject.OBJECT_COIN
     elif object_string[0] == "P":
       result.object_type = MapGridObject.OBJECT_PLAYER
+    elif object_string[0] == "S":
+      result.object_type = MapGridObject.OBJECT_SPIKES
     elif object_string[0] == "F":
       result.object_type = MapGridObject.OBJECT_ENEMY_FLYING
       result.enemy_id = int(object_string[2:])
@@ -197,6 +208,21 @@ class Level:
     #  be None (representing nothing) or a MapGridObject
     self.map_array = None
 
+  ## Gets the MapGridObject at given position in the map with map
+  #  boundary check.
+  #
+  #  @param x x position
+  #  @param y y position
+  #  @return MapGridObject at given position (can be also None) or
+  #          None if the coordinates are outside the map
+
+  def get_at(self, x, y):
+    if x < 0 or x >= self.width or y < 0 or y >= self.height:
+      return None
+
+    return self.map_array[x][y]
+
+
   def __init__(self):
     self.__init_attributes()
 
@@ -226,9 +252,26 @@ class Game:
 
 #-----------------------------------------------------------------------
 
+## Tile top layer image container.
+
+class TileTopImageContainer:
+  def __init__(self):
+    ## full image
+    self.image = None
+    ## subimage - left
+    self.left = None
+    ## subimage - center
+    self.center = None
+    ## subimage - right
+    self.right = None
+
+#-----------------------------------------------------------------------
+
 class Renderer:
   TILE_WIDTH = 200
   TILE_HEIGHT = 200
+  TOP_LAYER_OFFSET = 20
+  TOP_LAYER_LEFT_WIDTH = 23
 
   def __init_attributes(self):
     ## reference to a level being rendered
@@ -247,7 +290,7 @@ class Renderer:
     self._camera_y = 0
     ## contains images of tiles indexed by tile id, each item is a list
     #  where each item contains an image of one tile variant starting
-    #  from 1, index 0 contains an image for the tile top
+    #  from 1, index 0 contains a TileTopImageContainer
     self.tile_images = {}
     ## Contains the level background image
     self.background_image = None
@@ -257,6 +300,10 @@ class Renderer:
     #  (x1,y1,x2,y2)
     self.visible_tile_area = (0,0,0,0)
 
+    spikes_mask = pygame.image.load("resources/spikes_mask.bmp")
+    ## Contains the spikes image
+    self.spikes_image = prepare_image(pygame.image.load("resources/spikes.bmp"),transparency_mask = spikes_mask)
+
   ## Sets the level to be rendered.
   #
   #  @param level Level object
@@ -265,7 +312,7 @@ class Renderer:
     self._level = level
 
     # load the level background image:
-    self.background_image = prepare_image(pygame.image.load(("resources/background_" + self._level.background_name + ".bmp")))
+    self.background_image = prepare_image(pygame.image.load("resources/background_" + self._level.background_name + ".bmp"))
 
     self.background_repeat_times = int(math.ceil(self.screen_width / float(self.background_image.get_width())))
 
@@ -277,12 +324,35 @@ class Renderer:
     for tile in level.tiles:
       self.tile_images[tile[0]] = []
 
+      # tile top:
       top_mask = pygame.image.load(("resources/tile_" + tile[1] + "_top_mask.bmp"))
+      top_image = prepare_image(pygame.image.load(("resources/tile_" + tile[1] + "_top.bmp")),transparency_mask = top_mask)
 
-      self.tile_images[tile[0]].append(prepare_image(pygame.image.load(("resources/tile_" + tile[1] + "_top.bmp")),transparency_mask = top_mask)) # tile top
+      self.tile_images[tile[0]].append(TileTopImageContainer())
 
-      for variant_number in range(tile[2]):  # tile variants
+      self.tile_images[tile[0]][0].image = top_image
+      self.tile_images[tile[0]][0].left = top_image.subsurface(pygame.Rect(0,0,23,56)) # left
+      self.tile_images[tile[0]][0].center = top_image.subsurface(pygame.Rect(24,0,201,56)) # center
+      self.tile_images[tile[0]][0].right = top_image.subsurface(pygame.Rect(225,0,21,56)) # right
+
+      # tile variants:
+      for variant_number in range(tile[2]):
         self.tile_images[tile[0]].append(prepare_image(pygame.image.load("resources/tile_" + tile[1] + "_" + str(variant_number + 1) + ".bmp")))
+
+  ## Private method, checks if the tile at given position in the level
+  #  has a top layer (i.e. there is no other tile above it) and what
+  #  type.
+  #
+  #  @param x x position of the tile
+  #  @param y y position of the tile
+  #  @return a tree item tuple with boolean values (left, center, right)
+
+  def __get_top_layer(self, x, y):
+    center = not MapGridObject.is_tile(self._level.get_at(x, y - 1))
+    left = not MapGridObject.is_tile(self._level.get_at(x, y - 1)) and not MapGridObject.is_tile(self._level.get_at(x - 1, y)) and not MapGridObject.is_tile(self._level.get_at(x - 1, y - 1))
+    right = not MapGridObject.is_tile(self._level.get_at(x, y - 1)) and not MapGridObject.is_tile(self._level.get_at(x + 1, y)) and not MapGridObject.is_tile(self._level.get_at(x + 1, y - 1))
+
+    return (left,center,right)
 
   ## Renders the level (without GUI).
   #
@@ -304,8 +374,27 @@ class Renderer:
 
         if map_grid_object == None:
           continue
-        elif map_grid_object.object_type == MapGridObject.OBJECT_TILE:
-          result.blit(self.tile_images[map_grid_object.tile_id][1],(i * Renderer.TILE_WIDTH - self._camera_x, j * Renderer.TILE_HEIGHT - self._camera_y))
+        else:
+          x = i * Renderer.TILE_WIDTH - self._camera_x
+          y = j * Renderer.TILE_HEIGHT - self._camera_y
+
+          if map_grid_object.object_type == MapGridObject.OBJECT_TILE:
+            result.blit(self.tile_images[map_grid_object.tile_id][map_grid_object.tile_variant],(x,y))
+
+            top_layer = self.__get_top_layer(i,j)
+
+            if top_layer[0]:   # left
+              result.blit(self.tile_images[map_grid_object.tile_id][0].left,(x - Renderer.TOP_LAYER_LEFT_WIDTH,y - Renderer.TOP_LAYER_OFFSET))
+
+            if top_layer[1]:   # center
+              result.blit(self.tile_images[map_grid_object.tile_id][0].center,(x,y - Renderer.TOP_LAYER_OFFSET))
+
+            if top_layer[1]:   # right
+              result.blit(self.tile_images[map_grid_object.tile_id][0].right,(x + Renderer.TILE_WIDTH,y - Renderer.TOP_LAYER_OFFSET))
+
+          elif map_grid_object.object_type == MapGridObject.OBJECT_SPIKES:
+            result.blit(self.spikes_image,(x,y))
+
 
     return result
 
@@ -355,17 +444,17 @@ while 1:
 
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_RIGHT:
-        cam_x += 10
+        cam_x += 40
         print("r")
       elif event.key == pygame.K_LEFT:
         print("l")
-        cam_x -= 10
+        cam_x -= 40
       elif event.key == pygame.K_UP:
         print("u")
-        cam_y -= 10
+        cam_y -= 40
       elif event.key == pygame.K_DOWN:
         print("d")
-        cam_y += 10
+        cam_y += 40
 
       print(str(cam_x) + " " + str(cam_y))
       renderer.set_camera_position(cam_x,cam_y)
